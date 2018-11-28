023af3a4feddfb5a074860763bdc9f3391885d46
zeroichini <44456551+zeroichini@users.noreply.github.com>
2018-11-28 13:59:48 +0900

    Add files via upload

--- janken/javascripts/main.js
+++ janken/javascripts/main.js
@@ -17,30 +17,43 @@ const app = new Vue({
 	},
 
 	'app-body': {
-	    props: [],
+	    data: function(){
+		return {
+		    senarios: [
+			{
+			    id: 0,
+			    title: '第一章　宮沢健二',
+			    content: `雨ニモ負ケズ
+			    風ニモ負ケズ
+			    雪ニモ夏ノ暑サニモ負ケヌ
+			    丈夫ナ体ヲ持チ……`
+			},
+
+			{
+			    id: 1,
+			    title: '第二章　夏目漱石',
+			    content: `　山路を登りながら、こう考えた。
+			    　智に働けば角が立つ。情に棹させば流される。意地を通せば窮屈だ。とかくに人の世は住みにくい。
+			    　住みにくさが高じると、安い所へ引き越したくなる。どこへ越しても住みにくいと悟った時、詩が生れて、画が出来る……`
+			}
+		    ]
+		}
+	    },
+	    
 	    template: `
 		<div class="app-body">
-		    <app-body-senario></app-body-senario>
+		<app-body-senario v-for="senario in senarios" v-bind:key="senario.id" v-bind:senario="senario"></app-body-senario>
 		</div>
 		`,
 
 	    components: {
-
 		'app-body-senario': {
-		    data: function(){
-			return {
-			    title: '第一章　test',
-			    content: `　あいうえお
-			    kakikukeko
-			    сасисусесо
-			    `
-			}
-		    },
+		    props: ['senario'],
 		    
 		    template: `
 		        <div class="app-body-senario">
-			<h2>{{ title }}</h2>
-			<div class="app-body-senario-content">{{ content }}</div>
+			<h2>{{ senario.title }}</h2>
+			<div class="app-body-senario-content">{{ senario.content }}</div>
 		        </div>
 		    `
 		}
